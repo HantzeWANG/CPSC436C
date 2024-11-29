0d08b6243d98a3a0f7421da190db3a29fdba0424
Miranda-Tang <mirandatang0708@gmail.com>
2024-11-29 14:29:44 -0800

    Modified attendance table and removed unnecessary files

--- backend/people/serializers.py
+++ backend/people/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Attendance, Profile
 
 
-class AttendanceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Attendance
-        fields = ('id', 'profile', 'photo_url', 'timestamp')
-
-
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('profile_id', 'profile_name', 'profile_image', 'admin_id')
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Attendance
+        fields = ('id', 'profile', 'photo_url', 'timestamp')
