1dad66632fc6b2a1bb3f1a1bb54330b6be8dfdcb
Miranda-Tang <mirandatang0708@gmail.com>
2024-12-02 17:20:20 -0800

    Made nav bar collapsable and improved admin route logic

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import SideNav from './SideNav';
-import "./css/Layout.css"
+import React, { useState, useEffect } from "react";
+import SideNav from "./SideNav";
+import { Collapse, IconButton } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import "./css/Layout.css";
 
 const Layout = ({ children, showSideNav }) => {
-    return (
-        <div className="layout-container">
-            {showSideNav && <SideNav />}
-            <div className="content">{children}</div>
-        </div>
-    );
+	const [isNavOpen, setIsNavOpen] = useState(showSideNav);
+
+	useEffect(() => {
+		setIsNavOpen(showSideNav);
+	}, [showSideNav]);
+
+	return (
+		<div className="layout-container">
+			<div className="sidebar-container">
+				<Collapse
+					in={isNavOpen}
+					orientation="horizontal"
+					className="sidebar-collapse"
+				>
+					<div className="sidebar-content">
+						<SideNav />
+					</div>
+				</Collapse>
+
+				<IconButton
+					onClick={() => setIsNavOpen(!isNavOpen)}
+					className="toggle-button"
+				>
+					{isNavOpen ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+				</IconButton>
+			</div>
+
+			<main
+				className="main-content"
+				style={{ marginLeft: isNavOpen ? "240px" : "40px" }}
+			>
+				{children}
+			</main>
+		</div>
+	);
 };
 
 export default Layout;
